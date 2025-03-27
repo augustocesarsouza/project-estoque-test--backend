7040eac7734e91ad8f4e21c6e9b43ba553b53d87
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-03-27 08:36:40 -0400

    adicionei o properties FRONTEND-URL

--- src/main/java/test_estoque/estoque/MyConfiguration.java
+++ src/main/java/test_estoque/estoque/MyConfiguration.java
@@ -1,16 +1,19 @@
 package test_estoque.estoque;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MyConfiguration implements WebMvcConfigurer {
+    @Value("${FRONTEND-URL}")
+    private String frontendUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Aplica CORS a todas as rotas
-                .allowedOrigins("*") // URL do seu frontend
+                .allowedOrigins(frontendUrl) // URL do seu frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // Métodos permitidos
                 .allowedHeaders("*")  // Permite todos os headers
                 .allowCredentials(false);  // Se você precisar de cookies ou credenciais
