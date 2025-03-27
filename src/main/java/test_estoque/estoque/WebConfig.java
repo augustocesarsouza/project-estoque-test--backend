e3f75822f3500b82b9204c4c59608edf45b419e8
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-03-27 06:50:14 -0400

    coloquei o cors para resolver erro frontend

---
+++ src/main/java/test_estoque/estoque/WebConfig.java
@@ -0,0 +1,18 @@
+package test_estoque.estoque;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Aplica CORS a todas as rotas
+                .allowedOrigins("http://localhost:5173")  // URL do seu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Métodos permitidos
+                .allowedHeaders("*")  // Permite todos os headers
+                .allowCredentials(false);  // Se você precisar de cookies ou credenciais
+    }
+}
