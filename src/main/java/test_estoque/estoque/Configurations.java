72a3e2898eab022da7f2b311d5f1eff6f32e68d1
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-03-27 09:25:03 -0400

    arrumei configuration para arrumar cors

--- src/main/java/test_estoque/estoque/Configurations.java
+++ src/main/java/test_estoque/estoque/Configurations.java
@@ -30,20 +30,34 @@ public class Configurations implements WebMvcConfigurer {
     private String API_KEY;
     @Value("${API-SECRET}")
     private String API_SECRET;
+    @Value("${FRONTEND_URL}")
+    private String frontendUrl;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        // Configurando CORS diretamente aqui no Spring Security
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(request -> {
+                    var corsConfig = new org.springframework.web.cors.CorsConfiguration();
+                    corsConfig.addAllowedOrigin(frontendUrl);  // Permitir apenas o frontend
+                    corsConfig.addAllowedMethod(HttpMethod.GET);
+                    corsConfig.addAllowedMethod(HttpMethod.POST);
+                    corsConfig.addAllowedMethod(HttpMethod.PUT);
+                    corsConfig.addAllowedMethod(HttpMethod.DELETE);
+                    corsConfig.addAllowedHeader("*");
+                    corsConfig.setAllowCredentials(true);  // Permitir credenciais (cookies, tokens)
+                    return corsConfig;
+                }))
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
                         authorizationManagerRequestMatcherRegistry
                                 .requestMatchers(HttpMethod.GET, "/v1/public/**").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/v1/public/**").permitAll()
                                 .requestMatchers(HttpMethod.PUT, "/v1/public/**").permitAll()
                                 .requestMatchers(HttpMethod.DELETE, "/v1/public/**").permitAll()
-                                .anyRequest().authenticated())
+                                .anyRequest().authenticated())  // Exige autenticação para outras rotas
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(httpSecuritySessionManagementConfigurer ->
-                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));  // Desabilitar sessão
 
         return httpSecurity.build();
     }
